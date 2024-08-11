2336e0690a60d3d4076ef2d3a4dc26871178c83e
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-08-11 23:37:39 +0200

    Renaming

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,11 +6,11 @@
 use tauri::Builder;
 
 // Declare the `commands` module
-mod commands;
+mod file_explorer;
 
 fn main() {
     Builder::default()
-        .invoke_handler(tauri::generate_handler![commands::list_files_in_directory, commands::get_parent_directory, commands::get_directory_hierarchy, commands::get_home_directory, commands::list_drives])
+        .invoke_handler(tauri::generate_handler![file_explorer::list_files_in_directory, file_explorer::get_parent_directory, file_explorer::get_directory_hierarchy, file_explorer::get_home_directory, file_explorer::list_drives])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
