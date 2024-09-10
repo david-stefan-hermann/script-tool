5bf4296bc79a2236a116ca964d42729ff7558f5d
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-10 02:01:24 +0200

    added episode naming

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,7 +10,7 @@ use file_explorer::{
     change_directory, get_current_path, get_directory_hierarchy, go_to_parent_directory,
     list_drives, list_files_in_current_directory, FileExplorer, list_files_in_home_directory,
 };
-use file_operations::{rename_files_in_directory, adjust_episode_numbers_in_directory};
+use file_operations::{rename_files_in_directory, adjust_episode_numbers_in_directory, rename_episodes_with_titles, get_current_episode_names};
 
 use std::sync::{Arc, Mutex};
 use tauri::{generate_handler, Builder, Manager};
@@ -35,6 +35,8 @@ fn main() {
             list_files_in_home_directory,
             rename_files_in_directory,
             adjust_episode_numbers_in_directory,
+            rename_episodes_with_titles,
+            get_current_episode_names,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
