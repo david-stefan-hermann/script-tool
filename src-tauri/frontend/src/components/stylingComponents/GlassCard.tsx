2e48dd7861e2a589d583777ca90d670fd8cdffbc
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-12 22:59:15 +0200

    added title and title background image

--- src-tauri/frontend/src/components/stylingComponents/GlassCard.tsx
+++ src-tauri/frontend/src/components/stylingComponents/GlassCard.tsx
@@ -3,11 +3,20 @@ import "./glass-card.css";
 interface GlassCardProps {
     children: React.ReactNode;
     className?: string;
+    title?: string;
+    image?: string;
+    bgClass?: string;
 }
 
-export default function GlassCard({ children, className }: GlassCardProps) {
+export default function GlassCard({ children, className, title, image, bgClass }: GlassCardProps) {
     return (
-        <div className={`w-full h-full flex flex-col glass-card ${className}`}>
+        <div className={`w-full flex flex-col glass-card ${className}`}>
+            {title &&
+                <h1 className={`flex md:text-2xl text-xl bg-[#89CBDD] ${bgClass ? bgClass : "bg-center"}`}
+                    style={{ backgroundImage: `${image && "url(" + image + ")"}` }}
+                >{title}</h1>
+            }
+
             {children}
         </div>
     );
