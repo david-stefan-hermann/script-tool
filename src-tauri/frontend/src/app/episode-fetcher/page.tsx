9859558457049cf8f6105462e115131a11b0ceb6
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-12 22:59:41 +0200

    fixed layout

--- src-tauri/frontend/src/app/episode-fetcher/page.tsx
+++ src-tauri/frontend/src/app/episode-fetcher/page.tsx
@@ -1,9 +1,9 @@
-import EpisodeTitleFetcher from "@/components/fileOperations/EpisodeTitleFetcher";
+import EpisodeTitleFetcher from "@/components/episodeTitleFetcher/EpisodeTitleFetcher";
 import FallingPetalsBackground from "@/components/stylingComponents/FallingPetalsBackground";
 
 export default function Page() {
     return (
-        <main className="min-h-screen max-h-screen min-h-screen h-screen">
+        <main className="min-h-screen max-h-screen min-h-screen h-screen p-4">
             <FallingPetalsBackground animationSpeed={0.2} petalSize={0.8} petalMultiplier={1.5} />
             <EpisodeTitleFetcher />
         </main>
