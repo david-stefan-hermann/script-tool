f726638dc87924e222a1772623eb1691d748b325
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-12 03:41:51 +0200

    flower pedal background & Buttons

---
+++ src-tauri/frontend/src/components/stylingComponents/AnimatedButton.tsx
@@ -0,0 +1,12 @@
+import "./AnimatedButton.css";
+
+export function AnimatedButton({ text, onClick, image }: { text: string, onClick: () => void, image?: string }) {
+    return (
+        <a
+            className="flex justify-center text-center px-4 py-2 glow-on-hover align-middle lg:text-base text-sm flex-grow"
+            onClick={onClick}
+            style={{ backgroundImage: "url(" + image + ")" }}>
+            {text}
+        </a>
+    );
+}
