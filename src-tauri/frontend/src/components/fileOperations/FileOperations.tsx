25a7276aca2467dbde3b0b1c7dc9d97cbc0c8fe9
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-12 23:02:16 +0200

    switched select to buttons

--- src-tauri/frontend/src/components/fileOperations/FileOperations.tsx
+++ src-tauri/frontend/src/components/fileOperations/FileOperations.tsx
@@ -3,30 +3,34 @@
 import React, { useState } from 'react';
 import FileRenamer from './FileRenamer';
 import EpisodeNumberAdjuster from './EpisodeNumberAdjuster';
-import { BsInfo, BsInfoCircle, BsTools, BsWrench } from 'react-icons/bs';
+import { BsInfoCircle } from 'react-icons/bs';
 import EpisodeRenamer from './EpisodeRenamer';
 import GlassCard from '../stylingComponents/GlassCard';
+import ImageButtonSwitch from '../stylingComponents/ImageButtonSwitch';
 
 export default function FileOperations() {
     const [selectedOperation, setSelectedOperation] = useState<string>('fileRenamer');
     const [options, setOptions] = useState<FileOperationsOptions[]>();
 
+    type ToolOption = 'SR' | 'ADJ' | 'ER';  // Define the three possible options
+    const [toolOption, setToolOption] = useState<ToolOption>('SR');  // Initialize with one of the options
+
     useState(() => {
         setOptions([
             {
-                id: 'fileRenamer',
+                id: 'SR',
                 title: 'Suchen und Ersetzen',
                 description: 'Benenne Video Dateien in einem Verzeichnis um.',
                 option: <FileRenamer />
             },
             {
-                id: 'episodeNumberAdjuster',
+                id: 'ADJ',
                 title: 'Episodennummer anpassen',
                 description: 'Passe die Episodennummern in den Dateinamen an.',
                 option: <EpisodeNumberAdjuster />
             },
             {
-                id: 'episodeRenamer',
+                id: 'ER',
                 title: 'Episoden umbenennen',
                 description: 'Benenne Episoden in einem Verzeichnis um.',
                 option: <EpisodeRenamer />
@@ -35,32 +39,23 @@ export default function FileOperations() {
     });
 
     return (
-        <div className='flex flex-col h-full'>
-            <GlassCard>
-                <h1 className="flex md:text-2xl text-xl bg-bottom"
-                    style={{ backgroundImage: "url('/styling/buttons/red.jpg')" }}>Werkzeug</h1>
+        <>
+            <GlassCard title="Werkzeug" image='/styling/buttons/red.jpg' bgClass='bg-bottom'>
                 <div className="p-2 flex flex-col gap-2">
-                    <span className="mr-2"><BsTools className='h-5 w-5 align-sub inline text-blue-500 mr-2' />Werkzeug</span>
-                    <select
-                        id="operation-select"
-                        value={selectedOperation}
-                        onChange={(e) => setSelectedOperation(e.target.value)}
-                        className="border rounded px-2 py-1"
-                    >
-                        {options?.map((option) => (
-                            <option key={option.id} value={option.id}>
-                                {option.title}
-                            </option>
-                        ))}
-                    </select>
-                    <span className="mr-2"><BsInfoCircle className='h-5 w-5 align-sub inline text-blue-500 mr-2' />Beschreibung</span>
-                    <span>{options?.find((option) => option.id === selectedOperation)?.description}</span>
+                    <div className="flex gap-2">
+                        {/* Search and Replace */}
+                        <ImageButtonSwitch title="Suchen und Ersetzen" image='/logos/jikan_logo.png' selected={toolOption == "SR"} onClick={() => setToolOption("SR")} />
+                        {/* Episode Counter Adjuster */}
+                        <ImageButtonSwitch title="Episodenzahl anpassen" image='/logos/thetvdb_logo.jpg' selected={toolOption == "ADJ"} onClick={() => setToolOption("ADJ")} />
+                        {/* Episode Renamer */}
+                        <ImageButtonSwitch title="Episoden benennen" image='/logos/tvmaze_logo.png' selected={toolOption == "ER"} onClick={() => setToolOption("ER")} />
+                    </div>
+                    <span className="mr-2"><BsInfoCircle className='h-5 w-5 align-sub inline mr-2' />Beschreibung</span>
+                    <span>{options?.find((option) => option.id === toolOption)?.description}</span>
                 </div>
             </GlassCard>
-            <GlassCard>
-                {selectedOperation && options?.find((option) => option.id === selectedOperation)?.option}
-            </GlassCard>
-        </div>
+            {selectedOperation && options?.find((option) => option.id === toolOption)?.option}
+        </>
     );
 }
 
