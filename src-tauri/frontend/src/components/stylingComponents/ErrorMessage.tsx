e17960d5e891b133cd37a94e25c17c7fe4f7a1e5
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-14 00:41:38 +0200

    styling

--- src-tauri/frontend/src/components/stylingComponents/ErrorMessage.tsx
+++ src-tauri/frontend/src/components/stylingComponents/ErrorMessage.tsx
@@ -1,10 +1,17 @@
 import { BsExclamationCircleFill } from "react-icons/bs";
+import GlassCard from "./GlassCard";
 
 export default function ErrorMessage({ message }: { message: string }) {
     return (
-        <div className="flex items-center justify-center text-red-500">
-            <BsExclamationCircleFill className="mr-2" />
-            <span>{message}</span>
-        </div>
+        <GlassCard className="text-pink-600 gap-2">
+            <div className="flex flex-row justify-center p-2">
+                <div className='flex items-center'>
+                    <BsExclamationCircleFill className="h-5 w-5 align-sub inline mr-2" />
+                </div>
+                <div className='flex flex-grow'>
+                    {message}
+                </div>
+            </div>
+        </GlassCard>
     );
 }
