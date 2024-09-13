2f8730bcf86efc3a817596ea39174cabfc8b6446
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-13 15:04:48 +0200

    removed unused

--- src-tauri/frontend/src/app/page.tsx
+++ src-tauri/frontend/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="min-h-screen max-h-screen min-h-screen h-screen p-4 flex flex-row gap-4">
       <LayeredBackground />
       <FallingPetalsBackground animationSpeed={0.2} petalSize={0.8} petalMultiplier={1.5} />
-      <div className="col-start-1 max-h-screen overflow-hidden basis-1/3">
+      <div className="max-h-screen overflow-hidden basis-1/3">
         <FileExplorer />
       </div>
       <div className="max-h-screen overflow-hidden basis-1/3 flex flex-col gap-4">
