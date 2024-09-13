62e39a6afdf62a48ea7e7b556e1a60a8e50194ef
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-13 15:05:18 +0200

    extracted episode list

--- src-tauri/frontend/src/components/episodeTitleFetcher/EpisodeTitleFetcher.tsx
+++ src-tauri/frontend/src/components/episodeTitleFetcher/EpisodeTitleFetcher.tsx
@@ -2,31 +2,35 @@
 
 import { useEffect, useState } from 'react';
 import { SeasonedEpisodes, fetchAnimeEpisodeTitlesGroupedBySeason, fetchTVDBAnimeEpisodeTitles, fetchTVMAZEAnimeEpisodeTitlesBySeason } from '@/services/tauriService';
-import { Tooltip } from '@nextui-org/tooltip';
-import { emit } from '@tauri-apps/api/event'
 import { AnimatedButton } from '../stylingComponents/AnimatedButton';
 import GlassCard from '../stylingComponents/GlassCard';
 import ImageButtonSwitch from '../stylingComponents/ImageButtonSwitch';
 
-export default function EpisodeTitleFetcher() {
+interface EpisodeTitleFetcherProps {
+  seasons: SeasonedEpisodes[];
+  setSeasons: React.Dispatch<React.SetStateAction<SeasonedEpisodes[]>>; // Function to update the state
+  selectedSeason: number;
+  setSelectedSeason: React.Dispatch<React.SetStateAction<number>>; // Function to update the state
+  error: string | null;
+  setError: React.Dispatch<React.SetStateAction<string | null>>; // Function to update the state
+}
+
+export default function EpisodeTitleFetcher({ seasons, setSeasons, selectedSeason, setSelectedSeason, error, setError }: EpisodeTitleFetcherProps) {
   const [animeId, setAnimeId] = useState<number | null>(null);
   const [animeName, setAnimeName] = useState<string | null>(null);
   const [year, setYear] = useState<number | null>(null);
-  const [seasons, setSeasons] = useState<SeasonedEpisodes[]>([]);
-  const [selectedSeason, setSelectedSeason] = useState<number>(1);
-  const [error, setError] = useState<string | null>(null);
+  //const [seasons, setSeasons] = useState<SeasonedEpisodes[]>([]);
+  //const [selectedSeason, setSelectedSeason] = useState<number>(1);
   const apiSourceUrl = {
     "TVDB": { href: "https://www.thetvdb.com/", name: "TheTVDB" },
     "JIKA": { href: "https://jikan.moe/", name: "Jikan" },
     "TVMZ": { href: "https://www.tvmaze.com/", name: "TVMaze" },
   }
-  const [copySuccess, setCopySuccess] = useState('');  // For displaying the copy success message
-  const [sendSuccess, setsendSuccess] = useState('');
 
   // For handling the TVDB API key and the switch between Jikan and TheTVDB
   type ApiOption = 'TVDB' | 'JIKA' | 'TVMZ';  // Define the three possible options
   const [apiOption, setApiOption] = useState<ApiOption>('TVMZ');  // Initialize with one of the options
-  
+
   const [tvdbApiKey, setTvdbApiKey] = useState<string | null>(null); // Store API key for TVDB
 
   const fetchEpisodeTitles = async () => {
@@ -62,36 +66,6 @@ export default function EpisodeTitleFetcher() {
     }
   };
 
-  const handleCopy = () => {
-    const selectedTitles = seasons.find((season) => season.season === selectedSeason)?.titles.join('\n');
-    if (selectedTitles) {
-      navigator.clipboard.writeText(selectedTitles).then(() => {
-        setCopySuccess('Werte kopiert!');
-        setTimeout(() => {
-          setCopySuccess('');
-        }, 2000);
-      });
-    }
-  };
-
-  const handleSeasonChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedSeason(parseInt(e.target.value, 10));
-  };
-
-  const handleSendFetchedEpisodeTitles = async () => {
-    try {
-      const selectedTitles = seasons.find((season) => season.season === selectedSeason)?.titles;
-      await emit('send_episodes', { episodeTitles: selectedTitles });
-
-      setsendSuccess('Titel gesendet!');
-      setTimeout(() => {
-        setsendSuccess('');
-      }, 2000);
-    } catch (err) {
-      console.error(err);
-      setError(String(err));
-    }
-  }
 
   useEffect(() => {
     setError(null);
@@ -103,34 +77,32 @@ export default function EpisodeTitleFetcher() {
         <div className="flex flex-row gap-4">
           {/* API Selection Images */}
           <div>
-            <label className="block mb-2">API Quelle <a className="text-blue-500 hover:text-blue-600" href={apiSourceUrl[apiOption].href} target="_blank" rel="noopener noreferrer">{apiSourceUrl[apiOption].href}</a>
+            <label className="block mb-2">API Quelle <a className="text-purple-700 hover:text-purple-600 active:text-pink-600" href={apiSourceUrl[apiOption].href} target="_blank" rel="noopener noreferrer">{apiSourceUrl[apiOption].href}</a>
             </label>
             <div className="flex gap-2">
               {/* Jikan Button */}
-              <ImageButtonSwitch title="Jikan" image='/logos/jikan_logo.png' selected={apiOption == "JIKA"} onClick={() => setApiOption("JIKA")} />
+              <ImageButtonSwitch title="Jikan" image='/logos/jikan_logo.png' scale={0.8} selected={apiOption == "JIKA"} onClick={() => setApiOption("JIKA")} />
               {/* TVDB Button */}
-              <ImageButtonSwitch title="TheTVDB" image='/logos/thetvdb_logo.jpg' selected={apiOption == "TVDB"} onClick={() => setApiOption("TVDB")} />
+              <ImageButtonSwitch title="TheTVDB" image='/logos/thetvdb_logo.jpg' scale={0.8} selected={apiOption == "TVDB"} onClick={() => setApiOption("TVDB")} />
               {/* TVMaze Button */}
-              <ImageButtonSwitch title="TVMaze" image='/logos/tvmaze_logo.png' selected={apiOption == "TVMZ"} onClick={() => setApiOption("TVMZ")} />
+              <ImageButtonSwitch title="TVMaze" image='/logos/tvmaze_logo.png' scale={0.8} selected={apiOption == "TVMZ"} onClick={() => setApiOption("TVMZ")} />
             </div>
           </div>
-
-          {/* TVDB API Key Input (shown if TheTVDB is selected) */}
-          <div className='flex flex-col'>
-            {apiOption == "TVDB" && (
-              <div className='flex-grow'>
-                <label className="block mb-2">TVDB API Key</label>
-                <input
-                  type="text"
-                  value={tvdbApiKey || ''}
-                  onChange={(e) => setTvdbApiKey(e.target.value)}
-                  className="border rounded px-2 py-1 w-full"
-                  placeholder="Enter TVDB API Key"
-                />
-              </div>
-            )}
-          </div>
         </div>
+
+        {/* TVDB API Key Input (shown if TheTVDB is selected) */}
+        {apiOption == "TVDB" && (
+          <div>
+            <label className="block mb-2">TVDB API Key</label>
+            <input
+              type="text"
+              value={tvdbApiKey || ''}
+              onChange={(e) => setTvdbApiKey(e.target.value)}
+              className="border rounded px-2 py-1 w-full"
+              placeholder="required"
+            />
+          </div>
+        )}
 
         <div className="flex flex-row gap-4">
           {/* Anime ID Input */}
@@ -141,7 +113,7 @@ export default function EpisodeTitleFetcher() {
               value={animeId || ''}
               onChange={(e) => setAnimeId(e.target.value ? parseInt(e.target.value) : null)}
               className="border rounded px-2 py-1 w-full"
-              placeholder="Enter Anime ID (Optional)"
+              placeholder="optional"
             />
           </div>
 
@@ -153,7 +125,7 @@ export default function EpisodeTitleFetcher() {
               value={year || ''}
               onChange={(e) => setYear(e.target.value ? parseInt(e.target.value) : null)}
               className="border rounded px-2 py-1 w-full"
-              placeholder="Enter Anime Name"
+              placeholder="optional"
             />
           </div>
         </div>
@@ -167,7 +139,7 @@ export default function EpisodeTitleFetcher() {
               value={animeName || ''}
               onChange={(e) => setAnimeName(e.target.value || null)}
               className="border rounded px-2 py-1 w-full"
-              placeholder="Enter Anime Name"
+              placeholder="optional"
             />
           </div>
         </div>
@@ -176,41 +148,9 @@ export default function EpisodeTitleFetcher() {
         {error && <div className="text-red-500">{error}</div>}
 
         {/* Fetch Episode Titles Button */}
-        <div className='flex flex-row w-full gap-4'>
+        <div className='flex flex-row w-full'>
           <AnimatedButton text="Titel laden" onClick={fetchEpisodeTitles} image='/styling/buttons/button-purple.jpg' />
         </div>
-
-        {/* Season Dropdown and Text Area */}
-        {seasons.length > 0 && (
-          <>
-            <div className="">
-              <label className="block mb-2">Staffel</label>
-              <select
-                value={selectedSeason}
-                onChange={handleSeasonChange}
-                className="border rounded px-2 py-1 w-full"
-              >
-                {seasons.map((season) => (
-                  <option key={season.season} value={season.season}>
-                    {`Season ${season.season}`}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <textarea
-              className="border rounded px-2 py-1 w-full flex flex-grow overflowy-scroll"
-              value={seasons.find((season) => season.season === selectedSeason)?.titles.join('\n') || ''}
-              readOnly
-            />
-            <div className="flex flex-row gap-4">
-              {/* Send Episodes Button */}
-              <AnimatedButton text={sendSuccess ? sendSuccess : 'Übernehmen?'} onClick={handleSendFetchedEpisodeTitles} image='/styling/buttons/button-blue.jpg' />
-              {/* Copy Episodes Button */}
-              <AnimatedButton text={copySuccess ? copySuccess : 'Episodentitel kopieren'} onClick={handleCopy} image='/styling/buttons/button-purple.jpg' />
-            </div>
-          </>
-        )}
       </div>
     </GlassCard>
   );
---
+++ src-tauri/frontend/src/components/episodeTitleFetcher/EpisodeTitleFetcherList.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { useState } from "react";
+import { SeasonedEpisodes } from "@/services/tauriService";
+import { AnimatedButton } from "../stylingComponents/AnimatedButton";
+import { emit } from '@tauri-apps/api/event';
+import GlassCard from "../stylingComponents/GlassCard";
+
+interface EpisodeTitleFetcherListProps {
+    seasons: SeasonedEpisodes[];
+    selectedSeason: number;
+    setSelectedSeason: React.Dispatch<React.SetStateAction<number>>;
+    setError: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+export function EpisodeTitleFetcherList({ seasons, selectedSeason, setSelectedSeason, setError }: EpisodeTitleFetcherListProps) {
+    const [copySuccess, setCopySuccess] = useState('');  // For displaying the copy success message
+    const [sendSuccess, setsendSuccess] = useState('');
+    const handleCopy = () => {
+        const selectedTitles = seasons.find((season) => season.season === selectedSeason)?.titles.join('\n');
+        if (selectedTitles) {
+            navigator.clipboard.writeText(selectedTitles).then(() => {
+                setCopySuccess('Werte kopiert!');
+                setTimeout(() => {
+                    setCopySuccess('');
+                }, 2000);
+            });
+        }
+    };
+
+    const handleSeasonChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedSeason(parseInt(e.target.value, 10));
+    };
+
+    const handleSendFetchedEpisodeTitles = async () => {
+        try {
+            const selectedTitles = seasons.find((season) => season.season === selectedSeason)?.titles;
+            await emit('send_episodes', { episodeTitles: selectedTitles });
+
+            setsendSuccess('Titel gesendet!');
+            setTimeout(() => {
+                setsendSuccess('');
+            }, 2000);
+        } catch (err) {
+            console.error(err);
+            setError(String(err));
+        }
+    }
+
+    return (
+        <GlassCard className='h-full' title='Episoden' image='/styling/backsplash/orange.jpg'>
+            <div className='p-4 flex flex-col w-full h-full overflow-x-hidden gap-4'>
+
+                {/* Season Dropdown and Text Area */}
+                {seasons.length > 0 && (
+                    <>
+                        <div className="">
+                            <select
+                                value={selectedSeason}
+                                onChange={handleSeasonChange}
+                                className="border rounded px-2 py-1 w-full"
+                            >
+                                {seasons.map((season) => (
+                                    <option key={season.season} value={season.season}>
+                                        {`Staffel 0${season.season}`}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+
+                        <textarea
+                            className="border rounded px-2 py-1 w-full flex flex-grow overflowy-scroll"
+                            value={seasons.find((season) => season.season === selectedSeason)?.titles.join('\n') || ''}
+                            readOnly
+                        />
+                        <div className="flex flex-row gap-4">
+                            {/* Send Episodes Button */}
+                            <AnimatedButton text={sendSuccess ? sendSuccess : 'Übernehmen'} onClick={handleSendFetchedEpisodeTitles} image='/styling/buttons/button-blue.jpg' />
+                            {/* Copy Episodes Button */}
+                            <AnimatedButton text={copySuccess ? copySuccess : 'Kopieren'} onClick={handleCopy} image='/styling/buttons/button-purple.jpg' />
+                        </div>
+                    </>
+                )}
+            </div>
+        </GlassCard>
+    )
+}
