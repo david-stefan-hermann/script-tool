cc67e7d54d7dc0e417cd9c57c05ae3328e82e89c
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-13 02:38:19 +0200

    component for layered bg

---
+++ src-tauri/frontend/src/components/stylingComponents/LayeredBackground.tsx
@@ -0,0 +1,33 @@
+import "./layered-background.css";
+
+export default function LayeredBackground() {
+    return (
+        <div className={`layered-background`}>
+            { /*
+            <div className="w-screen h-screen relative overflow-hidden opacity-20">
+                <img
+                    src="/styling/background/clouds.webp"
+                    alt="Background"
+                    className="absolute bottom-0 left-1/2 transform -translate-x-1/2 min-w-full min-h-full object-cover"
+                />
+            </div>
+            */ }
+
+            <div className="w-full h-full fixed top-0 left-0 flex">
+                <img
+                    src="/styling/background/grid.svg"
+                    alt="Background Grid"
+                    className="w-full h-full"
+                />
+            </div>
+            <div className="w-full h-full relative overflow-hidden opacity-20">
+                <img
+                    src="/styling/background/landscape.jpg"
+                    alt="Background Landscape"
+                    className="absolute bottom-0 left-1/2 transform -translate-x-1/2 min-w-full min-h-full object-cover"
+                />
+            </div>
+
+        </div>
+    );
+}
