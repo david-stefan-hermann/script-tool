0b00c5fb4f477ddd6ac6f6f3b2ac5e11a0f9754f
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-13 15:05:32 +0200

    added scaling option

--- src-tauri/frontend/src/components/stylingComponents/ImageButtonSwitch.tsx
+++ src-tauri/frontend/src/components/stylingComponents/ImageButtonSwitch.tsx
@@ -1,5 +1,5 @@
 import { Tooltip } from '@nextui-org/tooltip';
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./glow-on-hover.css";
 import "./fire-border.css";
 
@@ -7,6 +7,7 @@ interface ApiOptionTooltipProps {
     title: string;
     image: string;
     selected: boolean;
+    scale?: number;
     onClick: () => void,
 }
 
@@ -14,14 +15,20 @@ const ApiOptionTooltip: React.FC<ApiOptionTooltipProps> = ({
     title,
     image,
     selected,
+    scale,
     onClick
 }) => {
+
     return (
         <Tooltip content={title} placement="top" className='bg-white px-2 rounded border border-gray-100'>
             <div
-                className={`flex glow-on-hover cursor-pointer h-20 w-20 ${selected && 'fire-border'}`}
+                className={`flex glow-on-hover cursor-pointer ${selected && 'fire-border'}`}
                 onClick={() => onClick()}
-                style={{ backgroundImage: `url(${image})` }}
+                style={{ 
+                    backgroundImage: `url(${image})`, 
+                    width: scale ? (scale * 4) + 'rem' : '4rem',
+                    height: scale ? (scale * 4) + 'rem' : '4rem',
+                }}
             >
             </div>
         </Tooltip>
