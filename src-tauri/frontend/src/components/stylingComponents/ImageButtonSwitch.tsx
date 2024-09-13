181708f37cfd5638285b439eb311b743bb9972e8
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-13 02:38:45 +0200

    styling

--- src-tauri/frontend/src/components/stylingComponents/ImageButtonSwitch.tsx
+++ src-tauri/frontend/src/components/stylingComponents/ImageButtonSwitch.tsx
@@ -1,6 +1,7 @@
 import { Tooltip } from '@nextui-org/tooltip';
 import React from 'react';
 import "./glow-on-hover.css";
+import "./fire-border.css";
 
 interface ApiOptionTooltipProps {
     title: string;
@@ -18,14 +19,10 @@ const ApiOptionTooltip: React.FC<ApiOptionTooltipProps> = ({
     return (
         <Tooltip content={title} placement="top" className='bg-white px-2 rounded border border-gray-100'>
             <div
-                className={`cursor-pointer h-16 w-16 ${selected && 'border-4 border-orange-500'}`}
+                className={`flex glow-on-hover cursor-pointer h-20 w-20 ${selected && 'fire-border'}`}
                 onClick={() => onClick()}
+                style={{ backgroundImage: `url(${image})` }}
             >
-                <img
-                    src={image}
-                    alt={title}
-                    className='h-full w-full'
-                />
             </div>
         </Tooltip>
     );
