20003850e6ac0e6d644970df7f41d3399b27048f
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-13 02:39:42 +0200

    added bg

--- src-tauri/frontend/src/app/episode-fetcher/page.tsx
+++ src-tauri/frontend/src/app/episode-fetcher/page.tsx
@@ -1,9 +1,11 @@
 import EpisodeTitleFetcher from "@/components/episodeTitleFetcher/EpisodeTitleFetcher";
 import FallingPetalsBackground from "@/components/stylingComponents/FallingPetalsBackground";
+import LayeredBackground from "@/components/stylingComponents/LayeredBackground";
 
 export default function Page() {
     return (
         <main className="min-h-screen max-h-screen min-h-screen h-screen p-4">
+            <LayeredBackground />
             <FallingPetalsBackground animationSpeed={0.2} petalSize={0.8} petalMultiplier={1.5} />
             <EpisodeTitleFetcher />
         </main>
--- src-tauri/frontend/src/app/page.tsx
+++ src-tauri/frontend/src/app/page.tsx
@@ -2,10 +2,12 @@ import FileExplorer from "@/components/fileExplorer/FileExplorer";
 import FilePreview from "@/components/fileExplorer/FilePreview";
 import FileOperations from "@/components/fileOperations/FileOperations";
 import FallingPetalsBackground from "@/components/stylingComponents/FallingPetalsBackground";
+import LayeredBackground from "@/components/stylingComponents/LayeredBackground";
 
 export default function Home() {
   return (
     <main className="min-h-screen max-h-screen min-h-screen h-screen p-4 flex flex-row gap-4">
+      <LayeredBackground />
       <FallingPetalsBackground animationSpeed={0.2} petalSize={0.8} petalMultiplier={1.5} />
       <div className="col-start-1 max-h-screen overflow-hidden basis-1/3">
         <FileExplorer />
