e75c6572af760138e19fdcf85780cd78cb3c59a9
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-13 03:06:31 +0200

    made darker button optional

--- src-tauri/frontend/src/components/stylingComponents/AnimatedButton.tsx
+++ src-tauri/frontend/src/components/stylingComponents/AnimatedButton.tsx
@@ -3,7 +3,7 @@ import "./glow-on-hover.css";
 export function AnimatedButton({ text, onClick, image }: { text: string, onClick: () => void, image?: string }) {
     return (
         <a
-            className="flex justify-center text-center px-4 py-2 glow-on-hover align-middle lg:text-base text-sm flex-grow"
+            className="flex justify-center text-center px-4 py-2 glow-on-hover glow-on-hover-darken align-middle lg:text-base text-sm flex-grow"
             onClick={onClick}
             style={{ backgroundImage: "url(" + image + ")" }}>
             {text}
