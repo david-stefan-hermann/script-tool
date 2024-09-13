dcd3f3a3db742a476beede2024083004c7d7e425
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-13 15:05:00 +0200

    moved some state variables here

--- src-tauri/frontend/src/app/episode-fetcher/page.tsx
+++ src-tauri/frontend/src/app/episode-fetcher/page.tsx
@@ -1,13 +1,45 @@
+"use client";
+
 import EpisodeTitleFetcher from "@/components/episodeTitleFetcher/EpisodeTitleFetcher";
 import FallingPetalsBackground from "@/components/stylingComponents/FallingPetalsBackground";
 import LayeredBackground from "@/components/stylingComponents/LayeredBackground";
+import { useState } from "react";
+import { SeasonedEpisodes } from '@/services/tauriService';
+import { EpisodeTitleFetcherList } from "@/components/episodeTitleFetcher/EpisodeTitleFetcherList";
 
 export default function Page() {
+    const [seasons, setSeasons] = useState<SeasonedEpisodes[]>([]);
+    const [selectedSeason, setSelectedSeason] = useState<number>(1);
+    const [error, setError] = useState<string | null>(null);
+
     return (
-        <main className="min-h-screen max-h-screen min-h-screen h-screen p-4">
+        <main className="min-h-screen max-h-screen min-h-screen h-screen p-4 flex flex-row gap-4">
+
             <LayeredBackground />
-            <FallingPetalsBackground animationSpeed={0.2} petalSize={0.8} petalMultiplier={1.5} />
-            <EpisodeTitleFetcher />
+            <FallingPetalsBackground
+                animationSpeed={0.2}
+                petalSize={0.8}
+                petalMultiplier={1.5}
+            />
+            <div className="max-h-screen overflow-hidden basis-1/2">
+                <EpisodeTitleFetcher
+                    seasons={seasons}
+                    setSeasons={setSeasons}
+                    selectedSeason={selectedSeason}
+                    setSelectedSeason={setSelectedSeason}
+                    error={error}
+                    setError={setError}
+                />
+            </div>
+            
+            <div className="max-h-screen overflow-hidden basis-1/2">
+                <EpisodeTitleFetcherList
+                    seasons={seasons}
+                    selectedSeason={selectedSeason}
+                    setSelectedSeason={setSelectedSeason}
+                    setError={setError}
+                />
+            </div>
         </main>
     );
 }
