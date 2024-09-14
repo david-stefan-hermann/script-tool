ef4f1152089f6cc537ea0153cf4ecbdd5fd5d4f2
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-14 02:28:52 +0200

    added fonts and colors

--- src-tauri/frontend/tailwind.config.ts
+++ src-tauri/frontend/tailwind.config.ts
@@ -1,4 +1,4 @@
-import {nextui} from '@nextui-org/theme';
+import { nextui } from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -10,10 +10,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        anime: ['var(--font-dela-gothic-one)'],
+        anime2: ['var(--font-bangers)'],
+      },
+      colors: {
+        inactive: '#57534e',
+        tools: '#374151',
+        controls: {
+          100: '#4338ca',
+          200: '#3730a3',   // hover
+          300: '#6366f1',   // active
+        },
+        info: '#60A5FA',
+        error: '#b91c1c',
+        media: '#16a34a',
+        dir: '#3b82f6',
       },
     },
   },
