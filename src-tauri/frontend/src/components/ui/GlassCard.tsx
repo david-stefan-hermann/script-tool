b4d9b7a1d0db5410509c63eb6d833f4285b891b4
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-15 21:21:55 +0200

    text outline and move to tailwind

--- src-tauri/frontend/src/components/ui/GlassCard.tsx
+++ src-tauri/frontend/src/components/ui/GlassCard.tsx
@@ -12,9 +12,22 @@ export default function GlassCard({ children, className, title, image, bgClass }
     return (
         <div className={`w-full flex flex-col glass-card ${className}`}>
             {title &&
-                <h1 className={`flex md:text-2xl text-xl font-anime bg-[#89CBDD] ${bgClass ? bgClass : "bg-center"}`}
-                    style={{ backgroundImage: `${image && "url(" + image + ")"}` }}
-                >{title}</h1>
+                <h1 className='relative justify-center flex py-6 px-4 md:text-2xl text-xl text-purple-400 text-center font-anime glass-card-border-bottom'
+                    style={{
+                        textShadow: '2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000'
+                    }}
+                >
+                    <span
+                        className={`absolute top-0 left-0 opacity-80 h-full w-full bg-cover
+                            ${bgClass ? bgClass : "bg-center"}
+                            `}
+                        style={{
+                            backgroundImage: `${image && "url(" + image + ")"}`,
+                            textShadow: '2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000'
+                        }}
+                    ></span>
+                    <span className="z-10">{title}</span>
+                </h1>
             }
 
             {children}
