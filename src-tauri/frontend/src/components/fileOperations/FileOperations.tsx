6e04823b952ea5ce6e2150b087161918d4749d0e
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-16 22:31:46 +0200

    added tool to put files in folders

--- src-tauri/frontend/src/components/fileOperations/FileOperations.tsx
+++ src-tauri/frontend/src/components/fileOperations/FileOperations.tsx
@@ -8,11 +8,13 @@ import GlassCard from '../layout/GlassCard';
 import ImageButtonSwitch from '../ui/ImageButtonSwitch';
 import { triggerRefresh } from '@/services/tauriService';
 import InfoMessage from '../common/InfoMessage';
+import FolderOrganizer from './FolderOrganizer';
 
 interface FileOperationsOptions {
     id: string;
     title: string;
     description: string;
+    image: string;
     option: React.ReactNode;
 }
 
@@ -27,19 +29,29 @@ export default function FileOperations() {
                 id: 'SR',
                 title: 'Suchen und Ersetzen',
                 description: 'Benenne alle Video Dateien in einem Verzeichnis um.',
+                image: '/styling/buttons/search.jpeg',
                 option: <FileRenamer />
             },
             {
                 id: 'ADJ',
                 title: 'Episodennummer anpassen',
                 description: 'Passe die Episodennummern in den Dateinamen an.',
+                image: '/styling/buttons/adjuster.jpg',
                 option: <EpisodeNumberAdjuster />
             },
             {
                 id: 'ER',
                 title: 'Episoden umbenennen',
                 description: 'Benenne Episoden in einem Verzeichnis. Drücke "Titel laden" um automatisch Titel zu laden.',
+                image: '/styling/buttons/rename2.jpg',
                 option: <EpisodeRenamer />
+            },
+            {
+                id: 'FO',
+                title: 'In Ordner Verschieben',
+                description: 'Verschiebe Dateien in Ordner. Oder ziehe Dateien aus Ordnern.',
+                image: '/styling/buttons/folder.jpg',
+                option: <FolderOrganizer />
             }
         ]);
     });
@@ -53,12 +65,15 @@ export default function FileOperations() {
             <GlassCard title="Werkzeug" image='/styling/backsplash/red.jpg' bgClass='bg-bottom'>
                 <div className="p-2 flex flex-col gap-2">
                     <div className="flex gap-2 justify-center">
-                        {/* Search and Replace */}
-                        <ImageButtonSwitch title="Suchen und Ersetzen" image='/styling/buttons/search.jpeg' selected={toolOption == "SR"} onClick={() => setToolOption("SR")} />
-                        {/* Episode Counter Adjuster */}
-                        <ImageButtonSwitch title="Episodenzahl anpassen" image='/styling/buttons/adjuster.jpg' selected={toolOption == "ADJ"} onClick={() => setToolOption("ADJ")} />
-                        {/* Episode Renamer */}
-                        <ImageButtonSwitch title="Episoden benennen" image='/styling/buttons/rename2.jpg' selected={toolOption == "ER"} onClick={() => setToolOption("ER")} />
+                        {options?.map((option) => (
+                            <ImageButtonSwitch
+                                key={option.id}
+                                title={option.title}
+                                image={option.image}
+                                selected={toolOption == option.id}
+                                onClick={() => setToolOption(option.id as ToolOption)}
+                            />
+                        ))}
                     </div>
                     <GlassCard>
                         <InfoMessage message={options?.find((option) => option.id === toolOption)?.description} />
---
+++ src-tauri/frontend/src/components/fileOperations/FolderOrganizer.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import GlassCard from "../layout/GlassCard";
+import { pullFilesFromFolders, putFilesInFolders } from "@/services/tauriService";
+import { AnimatedButton } from "../ui/AnimatedButton";
+import ErrorMessage from "../common/ErrorMessage";
+
+export default function FolderOrganizer() {
+
+    const [error, setError] = useState<string | null>(null);
+
+    function handlePutFilesInFolders() {
+        setError(null);
+        putFilesInFolders()
+            .then(() => {
+                console.log("Files moved successfully");
+            })
+            .catch((err) => {
+                console.error("Failed to move files:", err);
+                setError("Failed to move files: " + err);
+            });
+    }
+
+    function handlePullFilesFromFolders() {
+        setError(null);
+        pullFilesFromFolders()
+            .then(() => {
+                console.log("Files pulled successfully");
+            })
+            .catch((err) => {
+                console.error("Failed to pull files:", err);
+                setError("Failed to pull files: " + err);
+            });
+    }
+
+    return (
+        <GlassCard className='' title='Dateien Organisieren' image='/styling/backsplash/green.jpg'>
+            <div className="flex flex-col gap-2 p-2">
+
+                {error && <ErrorMessage message={error}></ErrorMessage>}
+
+                <AnimatedButton text="In Ordner packen" onClick={() => handlePutFilesInFolders()} image='/styling/buttons/button-blue.jpg' />
+                <AnimatedButton text="aus Ordnern ziehen" onClick={() => handlePullFilesFromFolders()} image='/styling/buttons/button-purple.jpg' />
+            </div>
+        </GlassCard>
+    );
+}
