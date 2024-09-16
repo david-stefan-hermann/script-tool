7ee0435b177237e827f1b6617c6c4f8357bdf40c
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-16 02:02:05 +0200

    support for multiple borders

--- src-tauri/frontend/src/components/ui/AnimatedButton.tsx
+++ src-tauri/frontend/src/components/ui/AnimatedButton.tsx
@@ -3,9 +3,12 @@ import "./glow-on-hover.css";
 export function AnimatedButton({ text, onClick, image }: { text: string, onClick: () => void, image?: string }) {
     return (
         <a
-            className="flex justify-center text-center px-4 py-2 glow-on-hover glow-on-hover-darken align-middle lg:text-base text-sm flex-grow"
+            className="flex justify-center text-center px-4 py-2 glow-on-hover glow-on-hover-darken align-middle lg:text-base text-sm flex-grow border-solid border-2 border-controls-100"
             onClick={onClick}
-            style={{ backgroundImage: "url(" + image + ")" }}>
+            style={{
+                backgroundImage: "url(" + image + ")",
+                textShadow: '1px 1px 0 #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000'
+            }}>
             {text}
         </a>
     );
