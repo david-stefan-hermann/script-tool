79966b0b9887dc92ae3c74d3ccc0814e76caca2e
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-16 04:12:45 +0200

    set media manipulator to home screen

--- src-tauri/frontend/src/app/page.tsx
+++ src-tauri/frontend/src/app/page.tsx
@@ -1,14 +1,5 @@
 import { redirect } from 'next/navigation';
-import FallingPetalsBackground from "@/components/layout/FallingPetalsBackground";
-import LayeredBackground from "@/components/layout/LayeredBackground";
 
 export default function Page() {
-    redirect('/windows/home');
-
-    return (
-        <main className="min-h-screen max-h-screen min-h-screen h-screen p-4 flex flex-row gap-4">
-            <LayeredBackground />
-            <FallingPetalsBackground animationSpeed={0.2} petalSize={0.8} petalMultiplier={1.5} />
-        </main>
-    );
+    redirect('/windows/media-manipulator');
 }
