4249f7088e09be72d6a6f6fc9ae09a9a2b842c1e
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-16 04:13:13 +0200

    code formatting

--- src-tauri/frontend/src/app/episode-fetcher/page.tsx
+++ src-tauri/frontend/src/app/episode-fetcher/page.tsx
@@ -15,7 +15,6 @@ export default function Page() {
 
     return (
         <main className="min-h-screen max-h-screen min-h-screen h-screen p-4 flex flex-row gap-4">
-
             <LayeredBackground />
             <FallingPetalsBackground
                 animationSpeed={0.2}
