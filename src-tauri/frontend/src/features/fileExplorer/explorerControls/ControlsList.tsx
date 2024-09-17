7e13e1a75f6b8d22806bda7890cc6397024b8c52
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-18 01:37:56 +0200

    added className param

--- src-tauri/frontend/src/features/fileExplorer/explorerControls/ControlsList.tsx
+++ src-tauri/frontend/src/features/fileExplorer/explorerControls/ControlsList.tsx
@@ -11,13 +11,14 @@ interface controlsList {
     title: string;
     isLoading: boolean;
     items: controlsListItems[];
+    className?: string;
     toggleDropUp: () => void;
 }
 
-export default function ControlsList({ title, isLoading, items, toggleDropUp }: controlsList) {
+export default function ControlsList({ title, isLoading, items, className, toggleDropUp }: controlsList) {
 
     return (
-        <GlassCard>
+        <GlassCard className={className}>
             <h2 className="flex flex-row justify-between text-xl font-bold px-2 py-1 gap-2">
                 <span className="flex flex-grow">{title}</span>
                 <span className='flex inline'>
@@ -32,7 +33,7 @@ export default function ControlsList({ title, isLoading, items, toggleDropUp }: 
             <ul className='flex-col w-full max-h-full overflow-x-hidden flex-grow'>
                 {items.map((item, index) => (
                     <li
-                        key={index}
+                        key={"list" + index}
                         className={`py-1 pl-2 flex cursor-pointer break-all text-md
                             ${index % 2 === 0 ? 'bg-white bg-opacity-0' : 'bg-white bg-opacity-10'}
                             hover:text-dir hover:bg-white-200 hover:bg-opacity-40 transition-colors duration-200 glass-card-border-top`}
