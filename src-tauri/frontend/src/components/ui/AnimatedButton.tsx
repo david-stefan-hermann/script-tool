3e4a5315525db7eb10448d3e583cafa1fc54d80e
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-09-20 02:29:42 +0200

    added react fragment

--- src-tauri/frontend/src/components/ui/AnimatedButton.tsx
+++ src-tauri/frontend/src/components/ui/AnimatedButton.tsx
@@ -10,15 +10,17 @@ interface AnimatedButtonProps {
 
 export function AnimatedButton({ text, onClick, image, download }: AnimatedButtonProps) {
     return (
-        <a
-            className="flex justify-center text-white text-center px-6 py-2 glow-on-hover glow-on-hover-darken align-middle lg:text-base text-sm border-solid border-1 overflow-hidden"
-            onClick={onClick}
-            style={{
-                backgroundImage: "url(" + image + ")",
-                textShadow: '1px 1px 0 #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000'
-            }}>
-            {download && <><BiSolidDownload className="h-full text-controls-100 lg:text-xl text-lg" />&nbsp;</>}
-            {text}
-        </a>
+        <>
+            <a
+                className="flex justify-center text-white text-center px-6 py-2 glow-on-hover glow-on-hover-darken align-middle lg:text-base text-sm border-solid border-1 overflow-hidden"
+                onClick={onClick}
+                style={{
+                    backgroundImage: "url(" + image + ")",
+                    textShadow: '1px 1px 0 #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000'
+                }}>
+                {download && <><BiSolidDownload className="h-full text-controls-100 lg:text-xl text-lg" />&nbsp;</>}
+                {text}
+            </a>
+        </>
     );
 }
